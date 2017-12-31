57d7f49eb54e00c200a6da7b7a7f58c34c252614
Israel Ogbole <iogbole@yahoo.com>
2017-12-31 01:59:46 +0000

    Update supplier_ability.rb

--- app/models/spree/supplier_ability.rb
+++ app/models/spree/supplier_ability.rb
@@ -13,7 +13,7 @@ module Spree
           product.supplier_ids.include?(user.supplier_id)
         end
         can [:admin, :index], Spree::Product
-        can [:admin, :index], Spree::Order
+        can [:admin, :index, :read], Spree::Order, Spree::Order.includes(:line_items).where(:line_items => { :product => { :supplier_id => user.supplier_id } })
         can [:admin, :manage, :read, :ready, :ship], Spree::Shipment, order: { state: 'complete' }, stock_location: { supplier_id: user.supplier_id }
         can [:admin, :create, :update], :stock_items
         can [:admin, :manage], Spree::StockItem, stock_location_id: user.supplier.stock_locations.pluck(:id)
